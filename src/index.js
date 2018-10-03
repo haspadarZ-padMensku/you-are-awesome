3afa09a90c766994da8f53cdcb5ebc3de13e91eb
haspadarZ-padMensku <gonchds@gmail.com>
2018-10-03 12:22:32 +0300

    6/10

--- src/index.js
+++ src/index.js
@@ -1,43 +1,76 @@
 const createEnumerableProperty = (propertyName) => propertyName;
 
 const createNotEnumerableProperty = (propertyName) => {
-    Object.defineProperty(Object.prototype,
-        propertyName,
-        {
-            get: () => value,
-            set: (newValue) => value = newValue,
-            enumerable: false,
-            configurable: true
-        });
-    return propertyName;
+	Object.defineProperty(Object.prototype,
+		propertyName,
+		{
+			get: () => value,
+			set: (newValue) => value = newValue,
+			enumerable: false,
+			configurable: true
+		});
+	return propertyName;
 };
 
 const createProtoMagicObject = () => {
-    let object = () => [];
-    object.prototype = object.__proto__;
-    return object;
+	let object = () => [];
+	object.prototype = object.__proto__;
+	return object;
 };
+
 let n = 0;
 const incrementor = () => {
-    n++;
-    Object.prototype.valueOf = () => n;
-    return incrementor;
+	n++;
+	Object.prototype.valueOf = () => n;
+	return incrementor;
 };
+
 let n1 = 0;
-const asyncIncrementor = () => {     
-    n1++;
-    let asyncFunction = () => {
-        n1++;
-        return new Promise (resolve => {
-            setTimeout(() => {
-                resolve();
-            }, 1000)
-        });
-    }
-    Object.prototype.valueOf = () => n1;
-    return asyncFunction;
+const asyncIncrementor = () => {
+	n1++;
+	let asyncFunction = () => {
+		n1++;
+		return new Promise(resolve => {
+			setTimeout(() => {
+				resolve();
+			}, 1000)
+		});
+	}
+	Object.prototype.valueOf = () => n1;
+	return asyncFunction;
 };
-const createIncrementer = () => { };
+
+const createIncrementer = () => {
+	let nextValue = {
+		value: 0,
+
+		[Symbol.iterator]() {
+			return this
+		},
+
+		next() {
+			this.value++;
+
+			if (this.current === undefined) {
+				this.current = 1;
+			}
+
+			if (this.current <= this.value) {
+				return {
+					done: false,
+					value: this.current++
+				};
+			} else {
+				delete this.current;
+				return {
+					done: true
+				};
+			}
+		}
+	}
+
+	return nextValue;
+};
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = () => { };
